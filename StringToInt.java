700a3555d6aa7aacefcc767764f3e416d0a80ec9
HansonHe23 <vinisher12@gmail.com>
2021-12-21 19:10:10 -0500

    considered negatives

--- StringToInt.java
+++ StringToInt.java
@@ -9,33 +9,42 @@ public class StringToInt{
     System.out.println(stringToInt("11111111111111"));
     System.out.println(stringToInt("wewe"));
     System.out.println(stringToInt("12345wewe6"));
+    System.out.println(stringToInt("-123465"));
   }
 
   public static int stringToInt(String s){
     int sum = 0;
     char index = 0;
+    int indexstart = 0;
     int indexstop = s.length();
+    int switcher = 1;
     for (int x = 0; x < s.length(); x++){
+      if (index == '-'){
+        indexstart = 1;
+        switcher = -1;
+      }
       index = s.charAt(x);
       //System.out.println(index);
-      if (index + 0 < 48 || index + 0 > 57 && indexstop == s.length()){
+      if ((index + 0 < 48 || index + 0 > 57) && indexstop == s.length()
+      && index != '-'){
         //System.out.println(indexstop);
-        indexstop = x;
+        indexstop = x + indexstart;
       }
     }
-    for (int x = 0; x < indexstop; x++){
+    for (int x = indexstart; x < indexstop; x++){
       //System.out.println(charToInt(s.charAt(s.length() - x - 1)));
       index = s.charAt(x);
       //System.out.println(sum);
-      if ((sum += charToInt(index) * Math.pow(10, indexstop - x - 1)) >
+      //-------------------Need to work on how to detect larger int ------------
+      /*if ((sum += charToInt(index) * Math.pow(10, indexstop - x - 1)) >
        Integer.MAX_VALUE ||
        (sum += charToInt(index) * Math.pow(10, indexstop - x - 1)) <
        Integer.MIN_VALUE) {
         throw new NumberFormatException("Not a Number");
-      }
+      }*/
       sum += charToInt(index) * Math.pow(10, indexstop - x - 1);
     }
-    return sum;
+    return sum * switcher;
   }
 
   public static int charToInt(char input){
