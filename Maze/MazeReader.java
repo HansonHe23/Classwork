3ab784f8e7ef1f17045a1fe671b00db6164d120c
HansonHe23 <vinisher12@gmail.com>
2022-02-19 10:32:25 -0500

    finished printing the char array

--- Maze/MazeReader.java
+++ Maze/MazeReader.java
@@ -2,37 +2,43 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Scanner;
+import java.util.*;
 public class MazeReader{
   public static void main(String[] args){
     try{
-      File stuff = new File(args[0]);
-      Scanner input = new Scanner(stuff);
-      String endResult = "";
-      while(input.hasNextLine()){
-        endResult +=  input.nextLine() + "\n";
-      }
-      System.out.println(endResult);
+      Character[][] lol = fileReader("Maze1.txt");
+      System.out.println(toString(lol));
     } catch (FileNotFoundException e){
     }
   }
 
-  /*public static int[][] fileReader(String filename){
-    File stuff = new File(filename);
-    Scanner input = new Scanner(filename);
-    while(input.hasNextLine()){
-
+  public static String toString(Character[][] array2){
+    String combine = "";
+    for (int x = 0; x < array2.length; x++){
+      for (int y = 0; y < array2[0].length; y++){
+        combine += array2[x][y];
+      }
+      if (x != array2.length - 1){
+        combine += "\n";
+      }
     }
+    return combine;
+  }
 
+  public static Character[][] fileReader(String filename)
+  throws FileNotFoundException{
     File stuff = new File(filename);
-    ArrayList<String> thing = new ArrayList<String>[0];
-    Scanner input = new Scanner(filename);
+    Scanner input = new Scanner(stuff);
+    ArrayList<String> array = new ArrayList<String>();
     while(input.hasNextLine()){
-      String line = input.nextLine();
-      thing.add(line);
+      array.add(input.nextLine());
     }
-    int[][] endArray = new int[thing.length][thing.get(0).length()];
-    for
-
-    return
-  }*/
+    Character[][] arrayChar = new Character[array.size()][(array.get(0)).length()];
+    for (int x = 0; x < array.size(); x++){
+      for (int y = 0; y < array.get(0).length(); y++){
+        arrayChar[x][y] = (array.get(x)).charAt(y);
+      }
+    }
+    return arrayChar;
+  }
 }
